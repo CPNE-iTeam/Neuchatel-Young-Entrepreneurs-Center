d078e5ea69f89214841c01c5375679e4c1e16af5
JdM <julien@rmbi.ch>
2025-06-29 21:57:25 +0200

    i

--- api/contact.php
+++ api/contact.php
@@ -1,7 +1,7 @@
 <?php
 
 $subject = "Contact NYEC";
-$message = "E-Mail : " . $_POST["mail"] . "----------" . $_POST["message"];
+$message = "E-Mail : " . $_POST["mail"] . "\n----------\n\n\n\n" . $_POST["message"];
 $to = "julien@rmbi.ch";
 
 mail($to, $subject, $message);
